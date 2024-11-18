907bd735f31993c0170d64dcf3c24976815db90d
Dalvelac <dalvelac@myuax.com>
2024-11-18 12:36:13 +0100

    Ejercicio 1

---
+++ Constitucion-estructura.c
@@ -0,0 +1,9 @@
+// Enunciado:     Defina una estructura Estudiante que contenga nombre (una cadena de caracteres), edad (un entero) y promedio (un flotante).
+
+#include <stdio.h>
+
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+};
